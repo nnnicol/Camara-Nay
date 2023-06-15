82ee7ec2322d12f79de9ff869b231a26992d9a76
Naydelin <nnpardo@espe.edu.ec>
2023-06-15 09:59:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/camara_nay/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.camara_nay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Bitmap imageBitmap;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void takePicture(View view) {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void saveImage(View view) {
+        if (imageBitmap != null) {
+            String savedImageURL = MediaStore.Images.Media.insertImage(
+                    getContentResolver(),
+                    imageBitmap,
+                    "Imagen capturada",
+                    "Descripción de la imagen"
+            );
+            if (savedImageURL != null) {
+
+            } else {
+
+            }
+        } else {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+
+        }
+    }
+
+}
